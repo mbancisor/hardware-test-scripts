fa4346a0b1bc03347c758d48bfefc4909a2098f9
slash <bancisor.mihai@gmail.com>
2019-09-09 19:55:19 +0300

    dual som

--- som_functions.py
+++ som_functions.py
@@ -25,116 +25,255 @@ def measure_phase(chan0,chan1):
     errorV = np.angle(chan0 * np.conj(chan1))*180/np.pi
     error = np.mean(errorV)
     return error
+def setup_hardware():
+    return 1
 
-def run_hardware_tests(TRXLO,sync,runs,uri):
+def run_hardware_tests(TRXLO,sync,runs,buf_len, uri1,uri2):
 
     # User configurable
     DDS_Freq = 4000000
 
     # Setup contexts
     try:
-        ctx = iio.Context(uri)
+        ctx1 = iio.Context(uri1)
     except:
-        raise Exception("No device found")
+        raise Exception("No device1 found")
+    try:
+        ctx2 = iio.Context(uri2)
+    except:
+        raise Exception("No device1 found")
 
-    ctrl_chipA = ctx.find_device("adrv9009-phy")
-    ctrl_chipB = ctx.find_device("adrv9009-phy-b")
-    txdac = ctx.find_device("axi-adrv9009-tx-hpc")
-    rxadc = ctx.find_device("axi-adrv9009-rx-hpc")
-    hmc7044 = ctx.find_device("hmc7044")
+    ctrl_chip1A = ctx1.find_device("adrv9009-phy")
+    ctrl_chip1B = ctx1.find_device("adrv9009-phy-b")
+    ctrl_chip2A = ctx2.find_device("adrv9009-phy")
+    ctrl_chip2B = ctx2.find_device("adrv9009-phy-b")
+
+
+    txdac1 = ctx1.find_device("axi-adrv9009-tx-hpc")
+    rxadc1 = ctx1.find_device("axi-adrv9009-rx-hpc")
+    txdac2 = ctx2.find_device("axi-adrv9009-tx-hpc")
+    rxadc2 = ctx2.find_device("axi-adrv9009-rx-hpc")
+
+    hmc7044_1 = ctx1.find_device("hmc7044")
+    hmc7044_1_car = ctx1.find_device("hmc7044-car")
+    hmc7044_2 = ctx2.find_device("hmc7044")
+    hmc7044_2_car = ctx2.find_device("hmc7044-car")
+    hmc7044_1_ext = ctx1.find_device("hmc7044-ext")
+
+    # request sync pulse
+    hmc7044_1_ext.attrs["sysref_request"].value = str(1)
+    # Check Sync status
+    # print(hmc7044_1._debug_attrs["status"].value)
 
     # Configure transceiver settings
-    LO = ctrl_chipA.find_channel("TRX_LO", True)
-    LO.attrs["frequency"].value = str(int(TRXLO))
-    LO = ctrl_chipB.find_channel("TRX_LO", True)
-    LO.attrs["frequency"].value = str(int(TRXLO))
+    LO1 = ctrl_chip1A.find_channel("TRX_LO", True)
+    LO1.attrs["frequency"].value = str(int(TRXLO))
+    LO1 = ctrl_chip1B.find_channel("TRX_LO", True)
+    LO1.attrs["frequency"].value = str(int(TRXLO))
+
+    LO2 = ctrl_chip2A.find_channel("TRX_LO", True)
+    LO2.attrs["frequency"].value = str(int(TRXLO))
+    LO2 = ctrl_chip2B.find_channel("TRX_LO", True)
+    LO2.attrs["frequency"].value = str(int(TRXLO))
     #
-    rx = ctrl_chipA.find_channel("voltage0")
-    rx.attrs['gain_control_mode'].value = 'slow_attack'
-    rx = ctrl_chipB.find_channel("voltage0")
-    rx.attrs['gain_control_mode'].value = 'slow_attack'
+    rx1 = ctrl_chip1A.find_channel("voltage0")
+    rx1.attrs['gain_control_mode'].value = 'slow_attack'
+    rx1 = ctrl_chip1B.find_channel("voltage0")
+    rx1.attrs['gain_control_mode'].value = 'slow_attack'
+
+    rx2 = ctrl_chip2A.find_channel("voltage0")
+    rx2.attrs['gain_control_mode'].value = 'slow_attack'
+    rx2 = ctrl_chip2B.find_channel("voltage0")
+    rx2.attrs['gain_control_mode'].value = 'slow_attack'
+
 
     if sync:
         # Calibrate
-        ctrl_chipA.attrs['calibrate_rx_phase_correction_en'] = True
-        ctrl_chipA.attrs['calibrate'] = True
-        ctrl_chipB.attrs['calibrate_rx_phase_correction_en'] = True
-        ctrl_chipB.attrs['calibrate'] = True
+        ctrl_chip1A.attrs['calibrate_rx_phase_correction_en'] = True
+        ctrl_chip1A.attrs['calibrate'] = True
+        ctrl_chip1B.attrs['calibrate_rx_phase_correction_en'] = True
+        ctrl_chip1B.attrs['calibrate'] = True
+
+        ctrl_chip2A.attrs['calibrate_rx_phase_correction_en'] = True
+        ctrl_chip2A.attrs['calibrate'] = True
+        ctrl_chip2B.attrs['calibrate_rx_phase_correction_en'] = True
+        ctrl_chip2B.attrs['calibrate'] = True
 
         # MCS
-        hmc7044.reg_write(0x5a,0)
+        #hmc7044_1.reg_write(0x5a,0)
         for k in range(12):
-            ctrl_chipA.attrs['multichip_sync'] = str(k)
-            ctrl_chipB.attrs['multichip_sync'] = str(k)
+            ctrl_chip1A.attrs['multichip_sync'] = str(k)
+            ctrl_chip1B.attrs['multichip_sync'] = str(k)
+
+        #hmc7044_2.reg_write(0x5a,0)
+        for k in range(12):
+            ctrl_chip2A.attrs['multichip_sync'] = str(k)
+            ctrl_chip2B.attrs['multichip_sync'] = str(k)
+
 
     # Enable all IQ channels
-    v0 = rxadc.find_channel("voltage0_i")
-    v1 = rxadc.find_channel("voltage0_q")
-    v2 = rxadc.find_channel("voltage1_i")
-    v3 = rxadc.find_channel("voltage1_q")
+    v0 = rxadc1.find_channel("voltage0_i")
+    v1 = rxadc1.find_channel("voltage0_q")
+    v2 = rxadc1.find_channel("voltage1_i")
+    v3 = rxadc1.find_channel("voltage1_q")
     v0.enabled = True
     v1.enabled = True
     v2.enabled = True
     v3.enabled = True
 
+    v4 = rxadc2.find_channel("voltage0_i")
+    v5 = rxadc2.find_channel("voltage0_q")
+    v6 = rxadc2.find_channel("voltage1_i")
+    v7 = rxadc2.find_channel("voltage1_q")
+    v4.enabled = True
+    v5.enabled = True
+    v6.enabled = True
+    v7.enabled = True
 
     # Create buffer for RX data
-    rxbuf = iio.Buffer(rxadc, 2**14, False)
+    rxbuf1 = iio.Buffer(rxadc1, buf_len, False)
+    rxbuf2 = iio.Buffer(rxadc2, buf_len, False)
     #
     # Enable single tone DDS
-    dds0_tx1 = txdac.find_channel('altvoltage0',True)
-    dds2_tx1 = txdac.find_channel('altvoltage2',True)
-    dds0_tx2 = txdac.find_channel('altvoltage8',True)
-    dds2_tx2 = txdac.find_channel('altvoltage10',True)
+    dds0_tx1_1 = txdac1.find_channel('altvoltage0',True)
+    dds2_tx1_1 = txdac1.find_channel('altvoltage2',True)
+    dds0_tx1_2 = txdac1.find_channel('altvoltage8',True)
+    dds2_tx1_2 = txdac1.find_channel('altvoltage10',True)
+
+    dds0_tx2_1 = txdac2.find_channel('altvoltage0',True)
+    dds2_tx2_1 = txdac2.find_channel('altvoltage2',True)
+    dds0_tx2_2 = txdac2.find_channel('altvoltage8',True)
+    dds2_tx2_2 = txdac2.find_channel('altvoltage10',True)
 
     # Turn all others off
-    dds1 = txdac.find_channel('altvoltage1',True)
-    dds1.attrs['raw'].value = str(0)
-    dds1.attrs['scale'].value = str(0)
+    dds1_1 = txdac1.find_channel('altvoltage1',True)
+    dds1_1.attrs['raw'].value = str(0)
+    dds1_1.attrs['scale'].value = str(0)
     for r in range(3,16):
-        dds1 = txdac.find_channel('altvoltage'+str(r),True)
-        dds1.attrs['scale'].value = str(0)
-        dds1.attrs['raw'].value = str(0)
+        dds1_1 = txdac1.find_channel('altvoltage'+str(r),True)
+        dds1_1.attrs['scale'].value = str(0)
+        dds1_1.attrs['raw'].value = str(0)
+
+    dds2_1 = txdac2.find_channel('altvoltage1', True)
+    dds2_1.attrs['raw'].value = str(0)
+    dds2_1.attrs['scale'].value = str(0)
+    for r in range(3, 16):
+        dds2_1 = txdac2.find_channel('altvoltage' + str(r), True)
+        dds2_1.attrs['scale'].value = str(0)
+        dds2_1.attrs['raw'].value = str(0)
 
     # Set frequency of enabled DDSs
-    dds0_tx1.attrs['raw'].value = str(1)
-    dds0_tx1.attrs['frequency'].value = str(DDS_Freq)
-    dds0_tx1.attrs['scale'].value = str(0.5)
-    dds0_tx1.attrs['phase'].value = str(90000)
-    dds2_tx1.attrs['raw'].value = str(1)
-    dds2_tx1.attrs['frequency'].value = str(DDS_Freq)
-    dds2_tx1.attrs['scale'].value = str(0.5)
-    dds2_tx1.attrs['phase'].value = str(0)
+    dds0_tx1_1.attrs['raw'].value = str(1)
+    dds0_tx1_1.attrs['frequency'].value = str(DDS_Freq)
+    dds0_tx1_1.attrs['scale'].value = str(0.5)
+    dds0_tx1_1.attrs['phase'].value = str(90000)
+    dds2_tx1_1.attrs['raw'].value = str(1)
+    dds2_tx1_1.attrs['frequency'].value = str(DDS_Freq)
+    dds2_tx1_1.attrs['scale'].value = str(0.5)
+    dds2_tx1_1.attrs['phase'].value = str(0)
 
-    dds0_tx2.attrs['raw'].value = str(1)
-    dds0_tx2.attrs['frequency'].value = str(DDS_Freq)
-    dds0_tx2.attrs['scale'].value = str(0.5)
-    dds0_tx2.attrs['phase'].value = str(90000)
-    dds2_tx2.attrs['raw'].value = str(1)
-    dds2_tx2.attrs['frequency'].value = str(DDS_Freq)
-    dds2_tx2.attrs['scale'].value = str(0.5)
-    dds2_tx2.attrs['phase'].value = str(0)
+    dds0_tx2_1.attrs['raw'].value = str(1)
+    dds0_tx2_1.attrs['frequency'].value = str(DDS_Freq)
+    dds0_tx2_1.attrs['scale'].value = str(0.5)
+    dds0_tx2_1.attrs['phase'].value = str(90000)
+    dds2_tx2_1.attrs['raw'].value = str(1)
+    dds2_tx2_1.attrs['frequency'].value = str(DDS_Freq)
+    dds2_tx2_1.attrs['scale'].value = str(0.5)
+    dds2_tx2_1.attrs['phase'].value = str(0)
 
+    dds0_tx1_2.attrs['raw'].value = str(1)
+    dds0_tx1_2.attrs['frequency'].value = str(DDS_Freq)
+    dds0_tx1_2.attrs['scale'].value = str(0.5)
+    dds0_tx1_2.attrs['phase'].value = str(90000)
+    dds2_tx1_2.attrs['raw'].value = str(1)
+    dds2_tx1_2.attrs['frequency'].value = str(DDS_Freq)
+    dds2_tx1_2.attrs['scale'].value = str(0.5)
+    dds2_tx1_2.attrs['phase'].value = str(0)
+
+    dds0_tx2_2.attrs['raw'].value = str(1)
+    dds0_tx2_2.attrs['frequency'].value = str(DDS_Freq)
+    dds0_tx2_2.attrs['scale'].value = str(0.5)
+    dds0_tx2_2.attrs['phase'].value = str(90000)
+    dds2_tx2_2.attrs['raw'].value = str(1)
+    dds2_tx2_2.attrs['frequency'].value = str(DDS_Freq)
+    dds2_tx2_2.attrs['scale'].value = str(0.5)
+    dds2_tx2_2.attrs['phase'].value = str(0)
+
+    # Stop continuous sysref 1
+    hmc7044_1_ext.reg_write(0x5, 0x2)
+    hmc7044_1_car.reg_write(0x5, 0x42)
+    hmc7044_1_car.reg_write(0x49, 0x0)
+    hmc7044_1_car.reg_write(0x5a, 0x1)
+    hmc7044_1.reg_write(0x49, 0x0)
+    hmc7044_1.reg_write(0x5a, 0x1)
+    hmc7044_1.reg_write(0x5, 0x43)
+    hmc7044_1.reg_write(0x5, 0x83)
+
+    # Stop continuous sysref 2
+    # hmc7044_2_ext.reg_write(0x5, 0x2)
+    hmc7044_2_car.reg_write(0x5, 0x42)
+    hmc7044_2_car.reg_write(0x49, 0x0)
+    hmc7044_2_car.reg_write(0x5a, 0x1)
+    hmc7044_2.reg_write(0x49, 0x0)
+    hmc7044_2.reg_write(0x5a, 0x1)
+    hmc7044_2.reg_write(0x5, 0x43)
+    hmc7044_2.reg_write(0x5, 0x83)
 
     # Collect data
     # reals0 = np.array([])
     # imags0 = np.array([])
     # reals1 = np.array([])
     # imags1 = np.array([])
-    phase_error = np.array([])
+    phase_error1 = np.array([])
+    phase_error2 = np.array([])
+    phase_error12 = np.array([])
+
+    i = 0
+    while rxadc1.reg_read(0x80000068) == 0:
+        rxadc1.reg_write(0x80000044, 0x8)
+        if (i == 50):
+            print("no HDL 1 sync")
+            break
+        i += 1
+    print("HDL 1 sync reg", rxadc1.reg_read(0x80000068))
+    print("tried ",i)
+
+    i = 0
+    while rxadc2.reg_read(0x80000068) == 0:
+        rxadc2.reg_write(0x80000044, 0x8)
+        if (i == 50):
+            print("no HDL 2 sync")
+            break
+        i += 1
+    print("HDL 2 sync reg", rxadc2.reg_read(0x80000068))
+    print("tried ",i)
+
+
 
     for i in range(runs):
-      rxbuf.refill()
-      data = rxbuf.read()
-      x = np.frombuffer(data,dtype=np.int16)
+
+
+      rxbuf1.refill()
+      rxbuf2.refill()
+
+      hmc7044_1_ext.attrs["sysref_request"].value = str(1)
+
+      data1 = rxbuf1.read()
+      data2 = rxbuf2.read()
+
+      x1 = np.frombuffer(data1,dtype=np.int16)
+      x2 = np.frombuffer(data2, dtype=np.int16)
       # reals0 = np.append(reals0,x[::4])
       # imags0 = np.append(imags0,x[1::4])
       # reals1 = np.append(reals1,x[2::4])
       # imags1 = np.append(imags1,x[3::4])
-      chan0 = x[0::4] + 1j*x[1::4]
-      chan1 = x[2::4] + 1j*x[3::4]
-      phase_error = np.append(phase_error,measure_phase(chan0,chan1))
-
+      chan1 = x1[2::4] + 1j*x1[3::4]
+      chan0 = x1[0::4] + 1j*x1[1::4]
+      chan3 = x2[2::4] + 1j*x2[3::4]
+      chan2 = x2[0::4] + 1j*x2[1::4]
+      phase_error1 = np.append(phase_error1, measure_phase(chan0, chan1))
+      phase_error2 = np.append(phase_error2, measure_phase(chan2, chan3))
+      phase_error12 = np.append(phase_error12, measure_phase(chan0, chan2))
     # # Plot
     # if do_plots:
     #     plt.plot(phase_error)
@@ -145,4 +284,4 @@ def run_hardware_tests(TRXLO,sync,runs,uri):
     #     plt.xlabel("Samples")
     #     plt.ylabel("Amplitude [dbFS]")
     #     plt.show()
-    return phase_error
+    return phase_error1, phase_error2, phase_error12
--- som_tests.py
+++ som_tests.py
@@ -6,63 +6,124 @@ import som_functions
 
 class TestADRV9009ZU11EG(unittest.TestCase):
 
-    measurements = 100
-    uri = "ip:192.168.1.217"
+    measurements = 10
+    buf_len = 2 ** 14
+    uri1 = "ip:192.168.1.60"
+    uri2 = "ip:192.168.1.61"
+
     prints = True
 
     def test_mean_over_time(self):
 
         LO = 1000000000
         sync = True
-        phase_diff1 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri)
-        var1 = np.var(phase_diff1)
-        m1 = np.mean(phase_diff1)
+        phase_diff_s1, phase_diff_s2, phase_diff_s12 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.buf_len,self.uri1,self.uri2)
+        var1_s1 = np.var(phase_diff_s1)
+        m1_s1 = np.mean(phase_diff_s1)
+        var1_s2 = np.var(phase_diff_s2)
+        m1_s2 = np.mean(phase_diff_s2)
+        var1_s12 = np.var(phase_diff_s12)
+        m1_s12 = np.mean(phase_diff_s12)
 
-        time.sleep(5)
+        time.sleep(1)
 
         sync = False
-        phase_diff2 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri)
-        var2 = np.var(phase_diff2)
-        m2 = np.mean(phase_diff2)
+        phase_diff2_s1, phase_diff2_s2, phase_diff2_s12 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.buf_len,self.uri1,self.uri2)
+        var2_s1 = np.var(phase_diff2_s1)
+        m2_s1 = np.mean(phase_diff2_s1)
+        var2_s2 = np.var(phase_diff2_s2)
+        m2_s2 = np.mean(phase_diff2_s2)
+        var2_s12 = np.var(phase_diff2_s12)
+        m2_s12 = np.mean(phase_diff2_s12)
+
 
         if self.prints:
-            print("Run 1 Variance %f (Degress^2)" % (var1))
-            print("Run 2 Variance %f (Degress^2)" % (var2))
-            print("Run 1 Mean Diff %f (Degress)" % (m1))
-            print("Run 2 Mean Diff %f (Degress)" % (m2))
+            print("SOM1")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s1))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s1))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s1))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s1))
+            print("Mean Diff %f (Degress)" % (m2_s1-m1_s1))
 
-        self.assertAlmostEqual(m1, m2, places=0)
+            print("SOM2")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s2))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s2))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s2))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s2))
+            print("Mean Diff %f (Degress)" % (m2_s2-m1_s2))
 
-        self.assertLess(var1, 0.05)
-        self.assertLess(var2, 0.05)
+            print("SOM12")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s12))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s12))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s12))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s12))
+            print("Mean Diff %f (Degress)" % (m2_s12-m1_s12))
+
+        #self.assertAlmostEqual(m1, m2, places=1)
+        self.assertLess(np.abs(m2_s1-m1_s1),0.2)
+
+        #self.assertLess(var1, 0.1)
+        #self.assertLess(var2, 0.1)
 
     def test_lo_change(self):
 
         LO = 1000000000
         sync = True
-        phase_diff1 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri)
-        var1 = np.var(phase_diff1)
-        m1 = np.mean(phase_diff1)
+        # phase_diff1 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri1,self.uri2)
+        #
+        # var1 = np.var(phase_diff1)
+        # m1 = np.mean(phase_diff1)
+        phase_diff_s1, phase_diff_s2, phase_diff_s12 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.buf_len,self.uri1,self.uri2)
+        var1_s1 = np.var(phase_diff_s1)
+        m1_s1 = np.mean(phase_diff_s1)
+        var1_s2 = np.var(phase_diff_s2)
+        m1_s2 = np.mean(phase_diff_s2)
+        var1_s12 = np.var(phase_diff_s12)
+        m1_s12 = np.mean(phase_diff_s12)
 
         LO = 2000000000
         sync = False
-        som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri)
+        som_functions.run_hardware_tests(LO,sync,self.measurements,self.buf_len,self.uri1,self.uri2)
 
         LO = 1000000000
-        phase_diff2 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri)
-        var2 = np.var(phase_diff2)
-        m2 = np.mean(phase_diff2)
+        # phase_diff2 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri1,self.uri2)
+        # phase_diff2 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.uri1,self.uri2)
+        # var2 = np.var(phase_diff2)
+        # m2 = np.mean(phase_diff2)
+        phase_diff2_s1, phase_diff2_s2, phase_diff2_s12 = som_functions.run_hardware_tests(LO,sync,self.measurements,self.buf_len,self.uri1,self.uri2)
+        var2_s1 = np.var(phase_diff2_s1)
+        m2_s1 = np.mean(phase_diff2_s1)
+        var2_s2 = np.var(phase_diff2_s2)
+        m2_s2 = np.mean(phase_diff2_s2)
+        var2_s12 = np.var(phase_diff2_s12)
+        m2_s12 = np.mean(phase_diff2_s12)
 
         if self.prints:
-            print("Run 1 Variance %f (Degress^2)" % (var1))
-            print("Run 2 Variance %f (Degress^2)" % (var2))
-            print("Run 1 Mean Diff %f (Degress)" % (m1))
-            print("Run 2 Mean Diff %f (Degress)" % (m2))
+            print("SOM1")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s1))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s1))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s1))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s1))
+            print("Mean Diff %f (Degress)" % (m2_s1 - m1_s1))
 
-        self.assertAlmostEqual(m1, m2, places=0)
+            print("SOM2")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s2))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s2))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s2))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s2))
+            print("Mean Diff %f (Degress)" % (m2_s2 - m1_s2))
 
-        self.assertLess(var1, 0.05)
-        self.assertLess(var2, 0.05)
+            print("SOM12")
+            print("Run 1 Variance %f (Degress^2)" % (var1_s12))
+            print("Run 2 Variance %f (Degress^2)" % (var2_s12))
+            print("Run 1 Mean Offset %f (Degress)" % (m1_s12))
+            print("Run 2 Mean Offset %f (Degress)" % (m2_s12))
+            print("Mean Diff %f (Degress)" % (m2_s12 - m1_s12))
+        #self.assertAlmostEqual(m1, m2, places=1)
+        self.assertLess(np.abs(m2_s1-m1_s1), 0.2)
+
+        #self.assertLess(var1, 0.1)
+        #self.assertLess(var2, 0.1)
 
 if __name__ == '__main__':
     unittest.main()
